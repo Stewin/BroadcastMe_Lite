925853061e0233b31e3e7f6e03ca1e2a29c4ea16
Stefan Winterberger <stefanwinterb88@hotmail.com>
2015-05-04 17:07:45 +0200

    Added Dummy Strings to MyMessages Fragment

--- app/src/main/java/ch/hslu/mobpro/projekt/broadcastmelite/MyMessagesFragment.java
+++ app/src/main/java/ch/hslu/mobpro/projekt/broadcastmelite/MyMessagesFragment.java
@@ -1,15 +1,22 @@
 package ch.hslu.mobpro.projekt.broadcastmelite;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Stefan on 21.04.2015.
  */
-public class MyMessagesFragment extends Fragment {
+public class MyMessagesFragment extends ListFragment {
+
+    private ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -17,7 +24,20 @@ public class MyMessagesFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_mymessages, container, false);
 
+        listView = (ListView) rootView.findViewById(R.id.list_item);
+
+        super.onActivityCreated(savedInstanceState);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+
         return rootView;
     }
 
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // do something with the data
+    }
 }
